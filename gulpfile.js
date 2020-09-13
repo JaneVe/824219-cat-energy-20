44cd989abc8092ab711f9f0dd1005b9b2d22184b
JaneVe <44479543+JaneVe@users.noreply.github.com>
2020-09-14 00:05:24 +0300

    Автоматизируем сборку личного проекта
    1. Собрала спрайт
    2. Оптимизировала контентные изображения в webp и добавила их в разметку.
    3. Добавила иконки соц сетей через спрайт
    4. Установила зависимости в package.json:
    - dell
    -gulp-csso
    -gulp-imagemin
    -gulp-rename
    -gulp-svgstore
    gulp-webp
    5. Добавила команды в gulpfile.js:
    - для минификации css
    - для оптимизации изображений
    - для задания имени
    - для создания спрайта
    - для очистки папки build
    6. Создала папку build. Добавила ее в гитигнор
    7. скопировала шрифты, изображения, js в папку build
    8. Добавила запуск build

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,75 @@ const sourcemap = require("gulp-sourcemaps");
 const less = require("gulp-less");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
+const csso = require("gulp-csso");
 const sync = require("browser-sync").create();
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const rename = require("gulp-rename");
+const svgstore = require("gulp-svgstore")
+const del = require("dell");
+
+//Build
+
+const clean = () => {
+  return del("build");
+};
+
+exports.clean = clean;
+
+const copy = () => {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico"
+  ], {
+      base: "source"
+  })
+ .pipe(gulp.dest("build"));
+ };
+
+ exports.copy = copy;
+
+ const build = gulp.series(
+  "clean",
+  "copy",
+  "css",
+  "sprite",
+  "html"
+ );
+
+ exports.build = build;
+
+//Images
+
+const sprite = () => {
+  return gulp.src("source/img/**/icon-*.svg")
+  .pipe(svgstore())
+  .pipe(rename("sprite.svg"))
+  .pipe(gulp.dest("build/img"));
+}
+
+exports.sprite = sprite;
+
+const images = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.mozjpeg({progressive: true}),
+    imagemin.svgo()
+    ]));
+}
+
+exports.images = images;
+
+const createWebp = () => {
+  return gulp.src("source/img/**/*.{png,jpg}")
+  .pipe(webp({quality: 90}))
+  .pipe(gulp.dest("source/img"));
+}
+
+exports.webp = createWebp;
 
 // Styles
 
@@ -16,8 +84,10 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("styles.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
